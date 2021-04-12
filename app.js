255c5a1f8006140e96fee02e87735ad4257ffb31
= <=>
2021-04-11 22:22:23 -0400

    menu

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ pizzas.map((item, index)=>{
     document.querySelector(".pizzaArea").append(pizzaItem)
     pizzaItem.querySelector(".pizza-item--img img").src = item.img
     pizzaItem.querySelector(".pizza-item--nome h1").innerHTML = item.name 
+    pizzaItem.querySelector(".pizza-item--info dd").innerHTML = item.description
 })
+
+function menuBurger(){
+    document.querySelector(".menuBurguer").addEventListener("click",(e)=>{
+        document.querySelector(".menu").classList.toggle("menuShow")
+   
+    })
+}
+
+menuBurger()
