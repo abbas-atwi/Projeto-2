4b4fcca569324f1f62c9a766b8f035dda5c1476c
= <=>
2021-04-12 00:07:37 -0400

    Update

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 pizzas.map((item, index)=>{
     let pizzaItem = document.querySelector(".pizza-item").cloneNode(true)
+
+    // Adicionou key-data para cada pizza item para identificador
+
     document.querySelector(".pizzaArea").append(pizzaItem)
+    let key = pizzaItem.setAttribute("data-key", index)
     pizzaItem.querySelector(".pizza-item--img img").src = item.img
     pizzaItem.querySelector(".pizza-item--nome h1").innerHTML = item.name 
+    pizzaItem.querySelector(".pizza-tem-preco span").innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector(".pizza-item--info dd").innerHTML = item.description
+
+    // Ao clicar no elemento a , ele vai pegar as informcoes
+    pizzaItem.querySelector("a").addEventListener("click",(e)=>{
+        e.preventDefault()
+        const el = e.target
+        let n = pizzaItem.querySelector(".pizza-item--nome h1").innerHTML = item.name
+
+        console.log(n)
+    })
 })
+
 
 function menuBurger(){
     document.querySelector(".menuBurguer").addEventListener("click",(e)=>{
-        document.querySelector(".menu").classList.toggle("menuShow")
+        let menu = document.querySelector(".menu")
+        menu.classList.toggle("menuShow")
+       setTimeout(()=>{
+           menu.style.display = 'flex'
+             menu.style.opacity = 1
+       },100)
    
     })
 }
+    if(window.innerWidth == 526){
+        menu.classList.remove("menuBurger")
+        window.onload()
+    }
 
 menuBurger()
--- pizzaJson.js
+++ pizzaJson.js
@@ -1,9 +1,9 @@
 let pizzas = [
     {id:1, name:"mussarela",img:"/images/pizza.png",price:20.97, description:"Uma pizza muito boa"},
-    {id:2, name:"Sorvete",img:"/images/pizza2.png",price:20.97, description:"Uma pizza muito boa"},
-    {id:3, name:"Calabresa",img:"/images/pizza3.png",price:20.97, description:"Uma pizza muito boa"},
+    {id:2, name:"Sorvete",img:"/images/pizza2.png",price:17.78, description:"Uma pizza muito boa"},
+    {id:3, name:"Calabresa",img:"/images/pizza3.png",price:47.98, description:"Uma pizza muito boa"},
     {id:4, name:"Quatro queijos",img:"/images/pizza4.png",price:20.97, description:"Uma pizza muito boa"},
     {id:5, name:"Frango com catupiry",img:"/images/pizza5.png",price:20.97, description:"Uma pizza muito boa"},
-    {id:6, name:"Atum",img:"/images/pizza6.png",price:20.97, description:"Uma pizza muito boa"},
-    {id:7, name:"Cogumelo",img:"/images/pizza7.png",price:20.97, description:"Uma pizza muito boa"},
+    {id:6, name:"Atum",img:"/images/pizza6.png",price:33, description:"Uma pizza muito boa"},
+    {id:7, name:"Cogumelo",img:"/images/pizza7.png",price:18, description:"Uma pizza muito boa"},
 ]
